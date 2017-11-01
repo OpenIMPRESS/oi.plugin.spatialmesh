f68d32e3cbb1a7382d86e557fd746844cc03ed8a
Emiel Harmsen <emielch@gmail.com>
2017-11-01 16:11:14 +0100

    init

---
+++ IdsSerializer.cs
@@ -0,0 +1,74 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Collections.Generic;
+using SysDiag = System.Diagnostics;
+using System.IO;
+using UnityEngine;
+
+namespace oi.plugin.spatialmesh {
+
+    public static class IdsSerializer {
+        /// <summary>
+        /// The mesh header consists of two 32 bit integers.
+        /// </summary>
+        private static int HeaderSize = sizeof(int) * 2;
+
+
+        public static byte[] Serialize(List<int> ids) {
+            byte[] data = null;
+
+            using (MemoryStream stream = new MemoryStream()) {
+                using (BinaryWriter writer = new BinaryWriter(stream)) {
+                    WriteIdList(writer, ids);
+
+                    stream.Position = 0;
+                    data = new byte[stream.Length];
+                    stream.Read(data, 0, data.Length);
+                }
+            }
+
+            return data;
+        }
+
+
+        public static IEnumerable<int> Deserialize(byte[] data) {
+            List<int> ids = new List<int>();
+
+            using (MemoryStream stream = new MemoryStream(data)) {
+                using (BinaryReader reader = new BinaryReader(stream)) {
+                    while (reader.BaseStream.Length - reader.BaseStream.Position >= HeaderSize) {
+                        ids.Add(reader.ReadInt32());
+                    }
+                }
+            }
+
+            return ids;
+        }
+
+
+        private static void WriteIdList(BinaryWriter writer, List<int> ids) {
+            SysDiag.Debug.Assert(writer != null);
+
+            // Write the mesh data.
+            WriteIdsHeader(writer, ids.Count);
+            WriteIds(writer, ids);
+        }
+
+        private static void WriteIdsHeader(BinaryWriter writer, int idsCount) {
+            SysDiag.Debug.Assert(writer != null);
+
+            writer.Write(0); // '0' announces ids list packet
+            writer.Write(idsCount);
+        }
+
+        private static void WriteIds(BinaryWriter writer, List<int> ids) {
+            SysDiag.Debug.Assert(writer != null);
+
+            foreach (int id in ids) {
+                writer.Write(id);
+            }
+        }
+
+    }
+}
---
+++ SpatialMeshTarget.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class SpatialMeshTarget : MonoBehaviour {
+
+    public Material surfaceMaterial;
+
+    public struct SurfaceObject
+    {
+        public int ID;
+        public int UpdateID;
+        public GameObject Object;
+        public MeshRenderer Renderer;
+        public MeshFilter Filter;
+    }
+
+    /// <summary>
+    /// When a mesh is created we will need to create a game object with a minimum 
+    /// set of components to contain the mesh.  These are the required component types.
+    /// </summary>
+    protected readonly Type[] componentsRequiredForSurfaceMesh =
+    {
+            typeof(MeshFilter),
+            typeof(MeshRenderer),
+            typeof(MeshCollider)
+        };
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public GameObject AddMeshObject(Mesh mesh, string objectName, int meshID = 0)
+    {
+        SurfaceObject surfaceObject = new SurfaceObject();
+        surfaceObject.ID = meshID;
+        surfaceObject.UpdateID = 0;
+
+        surfaceObject.Object = new GameObject(objectName, componentsRequiredForSurfaceMesh);
+        surfaceObject.Object.transform.position += transform.position;
+        surfaceObject.Object.transform.SetParent(transform);
+
+        surfaceObject.Filter = surfaceObject.Object.GetComponent<MeshFilter>();
+        surfaceObject.Filter.sharedMesh = mesh;
+
+        surfaceObject.Renderer = surfaceObject.Object.GetComponent<MeshRenderer>();
+        surfaceObject.Renderer.sharedMaterial = surfaceMaterial;
+
+        return surfaceObject.Object;
+    }
+
+    
+}
